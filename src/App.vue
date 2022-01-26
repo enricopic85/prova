05d86fa04065ff9c197d918b6ece78494d793c8c
enricopic85 <enricodev85@gmail.com>
2022-01-26 16:35:25 +0100

    push2

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,16 @@
 <template>
   <div id="app">
-    
+    <Title/>
   </div>
 </template>
 
 <script>
-
+import Title from './components/Title.vue'
 
 export default {
   name: 'App',
   components: {
-    
+    Title,
   }
 }
 </script>
